740aa9b34c34debf5a0118ab05abb175173575b4
hesham.mohsen <hesham.mohsen@qowwa.net>
2020-04-13 16:18:31 +0200

    refactor component based on type

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,8 +1,12 @@
 import React from "react";
 import useCountdownTimer from "./useCountdownTimer";
 
-function Event({ id, name, date, time, related_to, updateEvent }) {
+function UpcomingEvents({ id, name, date, time, related_to, started, updateEvent }) {
   const [days, hours, minutes, seconds] = useCountdownTimer(`${date} ${time}`);
+
+  if (days === "0" && started === false) {
+    updateEvent(id, { started: true });
+  }
 
   return (
     <div className="event mb-3">
@@ -24,7 +28,11 @@ function Event({ id, name, date, time, related_to, updateEvent }) {
       <div className="text-right">
         <i
           className="fas fa-trash-alt item-icon item-icon--fancy"
-          onClick={() => updateEvent(id, { is_deleted: true })}
+          onClick={() =>
+            updateEvent(id, {
+              is_deleted: true,
+            })
+          }
           title="delete event"
         ></i>
       </div>
@@ -32,4 +40,47 @@ function Event({ id, name, date, time, related_to, updateEvent }) {
   );
 }
 
-export default Event;
+function StartedEvent({ name, date, related_to }) {
+  return (
+    <div className="event mb-1 w-100">
+      <div className="event__header d-flex justify-content-between align-items-center">
+        <div className="event__time">{date}</div>
+        <div className="event__related">{related_to}</div>
+      </div>
+      <div className="event__name my-3">
+        <h2 title={name}>{name}</h2>
+      </div>
+    </div>
+  );
+}
+
+function TrashedEvent({ id, name, date, related_to, updateEvent }) {
+  return (
+    <div className="event mb-1 w-100">
+      <div className="event__header d-flex justify-content-between align-items-center">
+        <div className="event__time">{date}</div>
+        <div className="event__related">{related_to}</div>
+      </div>
+      <div className="event__name my-3">
+        <h2 title={name}>{name}</h2>
+      </div>
+      <div className="text-right m-0">
+        <i
+          className="fas fa-undo item-icon item-icon--fancy"
+          onClick={() => updateEvent(id, { is_deleted: false })}
+          title="undo event"
+        ></i>
+      </div>
+    </div>
+  );
+}
+
+export default function Event({ event, updateEvent, eventType }) {
+  const view = {
+    upcoming: () => <UpcomingEvents {...event} updateEvent={updateEvent} />,
+    started: () => <StartedEvent {...event} />,
+    trashed: () => <TrashedEvent {...event} updateEvent={updateEvent} />,
+  };
+
+  return view[eventType]();
+}
