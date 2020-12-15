304e2a9508ef9aea6cefabd08397d264bfaafb70
heshamMohsen <heshammohsen96@gmail.com>
2020-12-16 00:18:41 +0200

    detached db.json, removed action-types

--- src/constants/action-types.js
+++ src/constants/action-types.js
@@ -1,5 +1,4 @@
 // events
-export const SET_EVENTS = "SET_EVENTS";
 export const ADD_EVENT = "ADD_EVENT";
 export const UPDATE_EVENT = "UPDATE_EVENT";
 export const SET_ACTIVE_FILTER = "SET_ACTIVE_FILTER";
@@ -8,5 +7,4 @@ export const SHOW_ADD_EVENT = "SHOW_ADD_EVENT";
 // labels
 export const ADD_LABEL = "ADD_LABEL";
 export const DELETE_LABEL = "DELETE_LABEL";
-export const SET_LABELS = "SET_LABELS";
 export const IS_MODAL_OPEN = "IS_MODAL_OPEN";
